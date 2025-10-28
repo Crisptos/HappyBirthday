83b87bdf1129b409e050e0ff1b8666138d54838c
Crisptos <66401397+Crisptos@users.noreply.github.com>
2025-10-27 22:57:52 -0400

    cleaned up XML comments

--- HappyBirthday/src/main/java/com/happybirthday/BirthdayCardFragment.kt
+++ HappyBirthday/src/main/java/com/happybirthday/BirthdayCardFragment.kt
@@ -4,15 +4,9 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 
-/**
- * Simple fragment that inflates res/layout/fragment_birthday_card.xml
- */
 class BirthdayCardFragment : Fragment(R.layout.fragment_birthday_card) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        // Optional: wire up views here, e.g.
-        // val title = view.findViewById<TextView>(R.id.happy_bday_title)
-        // title.text = "Custom message"
     }
 }
